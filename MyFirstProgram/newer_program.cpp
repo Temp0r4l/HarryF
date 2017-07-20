ccf651c68a6b485e47835def8619ab22762ba3f7
Harry F <hfuqua8722@bths.edu>
2017-07-20 10:33:10 -0400

    saved work on functions, sleep, and OOP

---
+++ MyFirstProgram/newer_program.cpp
@@ -0,0 +1,30 @@
+#include <string>
+ 
+#include <iostream>
+ 
+using namespace std;
+ 
+class player {
+public:
+    player(); 
+    player(string inputName); 
+ 
+    string name; 
+     
+    void Greet();
+};
+
+player::player()
+{
+	name + "Default";
+}
+
+player::player(string theName)
+{
+	name = theName;
+}
+
+void player::Greet()
+{
+	cout << "Hey there " << name << "!" << endl;
+}
--- MyFirstProgram/program.cpp
+++ MyFirstProgram/program.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 using namespace std;
 
-int main()
+bool blastOff();
+
+bool blastOff()
 {
-	string myName;
-	cout << "Enter your name" << endl;
-	cin >> myName;
-	cout << "Hello " << myName <<endl;
+	char confirmation;
+	cout << "Enter 'y' to launch or 'n' to cancel." << endl;
+	cin >> confirmation;
+	
+	if (confirmation =='y')
+		return true;
+	else
+		return false;
+}
+
+int main() 
+{
+	bool launch;
+	launch = blastOff();
+	
+	if (launch == true)
+	{
+		for (int i = 10; i >0; i--)
+		{
+			cout << i << " seconds until launch" << endl;
+			
+			sleep((unsigned int)1);
+		}
+		cout << "The rocket has launched." << endl;
+	}
+	else
+	{
+		cout << "The launch has been aborted." << endl;
+	}
 }
